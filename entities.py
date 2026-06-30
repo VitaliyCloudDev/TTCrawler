b933742eb8e5077ed567f89d7f60cc43d38228ac
Vitaliy Shabanov <68377185+VitaliyCloudDev@users.noreply.github.com>
2026-06-30 08:06:32 +0000

    base damage update

--- entities.py
+++ entities.py
@@ -3,20 +3,24 @@ from random import choice
 class Entity:
     def __init__(self, name, symbol, x, y):
         self.name = name
+        self.symbol = symbol
         self.x = x
         self.y = y
-        self.symbol = symbol
+        self.hostile = True
+        self.alive = True
+
         self.move = None
-        self.damage = 10
         self.hp = 100
         self.maxhp = 100
         self.armor = 0
-        self.hostile = True
+
+        self.damage = 5
         self.speed = 10
         self.gold = 0
-        self.alive = True
+
         self.holding = None
         self.inventory = []
+        
     def get_hp(self):
         return f'{self.name} {self.hp}/{self.maxhp}'
     def attack(self, target):
