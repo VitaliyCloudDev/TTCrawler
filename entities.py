231b41117258ad78e8367320b8f06a3a15a7a38d
Vitaliy Shabanov <68377185+VitaliyCloudDev@users.noreply.github.com>
2026-06-30 09:14:27 +0000

    attack abilities

--- entities.py
+++ entities.py
@@ -1,4 +1,8 @@
-from random import choice
+from random import (
+    choice,
+    choices,
+    randint,
+)
 
 class Entity:
     def __init__(self, name, symbol, x, y):
@@ -16,21 +20,36 @@ class Entity:
         self.armor = 0
 
         self.damage = 5
-        self.speed = 10
+        self.speed = 5
         self.gold = 0
+        self.attacks = [['Удар',self.damage],
+                        ['Критический удар!',self.damage*2],
+                        ['Промах!',0]]
+        self.attacks_weight = [
+            60,
+            10,
+            30,
+        ]
 
         self.holding = None
         self.inventory = []
-        
+
     def get_hp(self):
         return f'{self.name} {self.hp}/{self.maxhp}'
+    
     def attack(self, target):
         print(f'{self.name} Атакует {target.get_hp()}!')
-        d = self.damage - target.armor
+        attack = choices(self.attacks,self.attacks_weight)[0]
+        d = attack[1] - target.armor
         if d < 0 :
-            d = 1
+            d = 0
         target.hp -= d
-        print(f'Нанесено {d} урона! {target.get_hp()}')
+        if target.hp <= 0:
+            target.alive = False
+        print(f'{attack[0]}')
+        if d != 0:
+            print(f'Нанесено {d} урона! {target.get_hp()}')
+
     def __repr__(self):
         return self.symbol
 
